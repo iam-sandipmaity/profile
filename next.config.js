31bd96b23d9d263fd65980ce4d6a328423cdbe74
iam-sandipmaity <social.sandipmaity+github@gmail.com>
2026-04-12 18:55:55 +0530

    refactor: convert from Next.js to React/Vite setup

--- next.config.js
+++
@@ -1,7 +0,0 @@
-// This file ensures Next.js uses the /pages directory for routing.
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
-
-module.exports = nextConfig;
